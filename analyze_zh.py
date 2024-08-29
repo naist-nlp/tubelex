04aca66c4cdb094acd7b01b8dff77bf94d537b74
Adam Nohejl <adam@nohejl.name>
2024-08-29 16:48:22 +0900

    Add logging to analyze_zh.py

--- analyze_zh.py
+++ analyze_zh.py
@@ -4,9 +4,11 @@ from tubelex import (
     UNIQUE_PATH_FMT,
     get_files_contents,
     )
-import hanzidentifier
 from collections import Counter
+from typing import Optional
+import sys
 from tqdm import tqdm
+import hanzidentifier as hzid
 
 '''
 Usage (for LZMA compression):
@@ -15,11 +17,11 @@ python analyze_zh.py -z
 '''
 
 HANZI_ID2DESC = {
-    hanzidentifier.UNKNOWN: 'Unknown',
-    hanzidentifier.BOTH: 'Both',
-    hanzidentifier.TRADITIONAL: 'Traditional',
-    hanzidentifier.SIMPLIFIED: 'Simplified',
-    hanzidentifier.MIXED: 'Mixed',
+    hzid.UNKNOWN: 'Unknown',
+    hzid.BOTH: 'Both',
+    hzid.TRADITIONAL: 'Traditional',
+    hzid.SIMPLIFIED: 'Simplified',
+    hzid.MIXED: 'Mixed',
     }
 
 
@@ -28,47 +30,83 @@ def parse() -> argparse.Namespace:
         'Analyze Chinese variants'
         ))
     Storage.add_arg_group(parser, 'Compression options', zip_suffix=True)
-    parser.add_argument('--strict', action='store_true')
+    parser.add_argument('--strict', '-s', action='store_true')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='Log info about non-strict identification to stdout.')
     return parser.parse_args()
 
 
-def identify_majority(s: str, min_prop: float = 0.95, max_ignore: int = 1) -> int:
+def get_example(lines: list[str], kind: int) -> Optional[str]:
+    if (s := next((s for s in lines if hzid.identify(s) == kind), None)):
+        if kind == hzid.MIXED:
+            tcs = ''.join(c for c in s if hzid.identify(c) == hzid.TRADITIONAL)
+            scs = ''.join(c for c in s if hzid.identify(c) == hzid.SIMPLIFIED)
+            return f'T={tcs}, S={scs} in {s}'
+        cs = ''.join(c for c in s if hzid.identify(c) == kind)
+        return f'{cs} in {s}'
+    return None
+
+
+def identify_majority(
+        s: str,
+        name: Optional[str] = None,  # log if MIXED
+        min_prop: float = 0.95,
+        max_ignore: int = 1,
+        ) -> int:
     '''
     Return the majority category (by counting lines).
     Can be one of Traditional/Simplified, if the proportion (including lines that can
     be both) is >= min_prop OR the number of lines that belongs to the other category
     (not including lines that can be both) is <= max_ignore.
     '''
-    c = Counter(map(hanzidentifier.identify, s.splitlines()))
-    n = c.total() - c[hanzidentifier.UNKNOWN]
+    lines = s.splitlines()
+    c = Counter(map(hzid.identify, lines))
+    n = c.total() - c[hzid.UNKNOWN]
     if not n:
-        return hanzidentifier.UNKNOWN
-    both = c[hanzidentifier.BOTH]
+        return hzid.UNKNOWN
+    both = c[hzid.BOTH]
     if both == n:
-        return hanzidentifier.BOTH
-    trad = c[hanzidentifier.TRADITIONAL]
-    simp = c[hanzidentifier.SIMPLIFIED]
+        return hzid.BOTH
+    trad = c[hzid.TRADITIONAL]
+    simp = c[hzid.SIMPLIFIED]
     trad_prop = (trad + both) / n
     simp_prop = (simp + both) / n
     if (trad_prop > simp_prop) and ((trad_prop >= min_prop) or (simp <= max_ignore)):
-        return hanzidentifier.TRADITIONAL
+        return hzid.TRADITIONAL
     if (simp_prop > trad_prop) and ((simp_prop >= min_prop) or (trad <= max_ignore)):
-        return hanzidentifier.SIMPLIFIED
-    return hanzidentifier.MIXED
+        return hzid.SIMPLIFIED
+
+    if name is not None:
+        ext = get_example(lines, hzid.TRADITIONAL)
+        exs = get_example(lines, hzid.SIMPLIFIED)
+        exm = get_example(lines, hzid.MIXED)
+        print(
+            f'{name}: {n} lines = {trad} Trad. + {simp} Simp. + '
+            f'{both} both + {c[hzid.MIXED]} Mixed, e.g.:\n'
+            f'- T: {ext}\n'
+            f'- S: {exs}\n'
+            f'- M: {exm}\n'
+            )
+
+    return hzid.MIXED
 
 
 def main() -> None:
     args = parse()
     storage = Storage.from_args(args)
 
-    identify = hanzidentifier.identify if args.strict else identify_majority
+    identify = hzid.identify if args.strict else identify_majority
     with get_files_contents(
         UNIQUE_PATH_FMT % 'zh', storage,
         ) as files_contents:
         files, iter_contents = files_contents
+        id_iter = (
+            map(identify, iter_contents(), files) if (args.verbose and not args.strict)
+            else map(identify, iter_contents())
+            )
         zh_id_counts = Counter(tqdm(
             desc='Identifiying Chinese variant in unique files',
-            iterable=map(identify, iter_contents()),
+            iterable=id_iter,
             total=len(files)
             ))
     with open('corpus/variants-zh.csv', 'w') as f:
